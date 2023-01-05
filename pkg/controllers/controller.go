dc741deef445c06307766f5544f663043d0245bd
IlyaMudritskiy <ilya.mudritskiy@gmail.com>
2023-01-05 20:38:01 +0000

    Added basic layout of controller functions

---
+++ pkg/controllers/controller.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"net/http"
+)
+
+func CreateBook(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func GetBooks(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func GetBookById(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func UpdateBook(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func DeleteBook(writer http.ResponseWriter, request *http.Request) {
+
+}
