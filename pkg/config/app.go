c29e96e581f54a51cd10500977c0c643ad1674cb
IlyaMudritskiy <ilya.mudritskiy@gmail.com>
2023-01-05 21:49:07 +0000

    Added database connection handler

---
+++ pkg/config/app.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ilyamudritskiy/bookstore_management/pkg/settings"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func ConnectDB() {
+
+	var db_credentials = fmt.Sprintf(
+		"%s:%s/%s?charset=utf8&parseTime=True&loc=Local",
+		settings.DB_USERNAME,
+		settings.DB_PASSWD,
+		settings.DB_TABLENAME)
+
+	db, err := gorm.Open("mysql", db_credentials)
+
+	if err != nil {
+		panic(err)
+	}
+
+	DB = db
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
