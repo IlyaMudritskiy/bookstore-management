b71ce138f130c0919fbe12af2564e37b6e9685f8
IlyaMudritskiy <ilya.mudritskiy@gmail.com>
2023-01-08 20:15:02 +0000

    Finished main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/ilyamudritskiy/bookstore_management/pkg/routes"
+)
+
+func main() {
+	var router = mux.NewRouter()
+	routes.RegisterRoutes(router)
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe("localhost:9010", router))
+}
